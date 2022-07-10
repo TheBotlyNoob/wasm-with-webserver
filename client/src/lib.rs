ea21eb37c15d9d055476574e73548cb9fdc5b2d3
Jay Jackson <jj@jj.is-a.dev>
2022-07-10 06:02:37 +0000

    remove nightly

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -5,6 +5,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
 pub fn main() {
+    #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
     web_sys::window()
