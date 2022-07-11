1a0daba68486d0a559b224d4b84e116683840bd1
Jay Jackson <jj@jj.is-a.dev>
2022-07-11 03:32:52 +0000

    much better code

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,46 +1,20 @@
-use axum::{http::header, routing::get, Router};
+use axum::{routing::get, Router};
 
-#[derive(rust_embed::RustEmbed)]
-#[folder = "$OUT_DIR"]
-struct Frontend;
+mod frontend;
+use frontend::frontend;
 
 #[tokio::main]
 async fn main() {
-    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
-    println!("listening on http://localhost:3000");
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_e| String::from("8080"))
+        .parse::<u16>()
+        .expect("PORT must be a valid port");
+
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
+    println!("listening on http://localhost:{port}/");
 
     axum::Server::bind(&addr)
-        .serve(
-            Router::new()
-                .route(
-                    "/client_bg.wasm",
-                    get(|| async {
-                        (
-                            [(header::CONTENT_TYPE, "application/wasm")],
-                            Frontend::get("client_bg.wasm").unwrap().data,
-                        )
-                    }),
-                )
-                .route(
-                    "/client.js",
-                    get(|| async {
-                        (
-                            [(header::CONTENT_TYPE, "text/javascript")],
-                            Frontend::get("client.js").unwrap().data,
-                        )
-                    }),
-                )
-                .route(
-                    "/",
-                    get(|| async {
-                        (
-                            [(header::CONTENT_TYPE, "text/html")],
-                            include_str!("../../client/src/index.html"),
-                        )
-                    }),
-                )
-                .into_make_service(),
-        )
+        .serve(Router::new().fallback(get(frontend)).into_make_service())
         .await
         .unwrap();
 }
